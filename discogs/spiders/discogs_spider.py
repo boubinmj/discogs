5acc822288df8d77b11d074470bc78b038564f54
boubinmj <boubinmj@miamioh.edu>
2021-06-01 07:53:11 -0400

    reverted to working state for 2014+

--- discogs/spiders/discogs_spider.py
+++ discogs/spiders/discogs_spider.py
@@ -5,10 +5,10 @@ import re
 class DiscogsSpider(Spider):
     name = "discogs_spider"
     allowed_urls = ['https://www.wikipedia.org']
-    start_urls = ['https://en.wikipedia.org/wiki/List_of_Billboard_200_number-one_albums_of_2020']
+    start_urls = ['https://en.wikipedia.org/wiki/List_of_Billboard_200_number-one_albums_of_2014']
 
     def parse(self, response):
-        page_urls = [f'https://en.wikipedia.org/wiki/List_of_Billboard_200_number-one_albums_of_{i}' for i in range(2010, 2021)]
+        page_urls = [f'https://en.wikipedia.org/wiki/List_of_Billboard_200_number-one_albums_of_{i}' for i in range(2010, 2015)]
 
         for url in page_urls:
             yield Request(url=url, callback=self.parse_results_page)
@@ -17,21 +17,16 @@ class DiscogsSpider(Spider):
     def parse_results_page(self, response):
 
 
-        try:
-            rows = response.xpath('//table[@class="wikitable plainrowheaders"]//tr')
-        except:
+        rows = response.xpath('//table[@class="wikitable plainrowheaders"]//tr')
+        if(rows == []):
             rows = response.xpath('//table[2]//tr')
 
         for row in rows[1:]:
 
-            try:
-                album = row.xpath('./td[1]//a/text()').extract_first()
-                if(album is None):
-                    continue
-            except:
-                row.xpath('./td[2]//a/text()').extract_first()
-                if(album is None):
-                    continue
+            album = row.xpath('./td[1]//a/text()').extract_first()
+            if(album is None):
+                continue
+
             
             artist = row.xpath('./td[2]//a/text()').extract_first()
             if(artist is None):
@@ -40,7 +35,8 @@ class DiscogsSpider(Spider):
             try:
                 issue_date = row.xpath('./th/text()').extract_first().strip()
             except:
-                issue_date = row.xpath('./td[1]//a/text()').extract_first().strip()
+                issue_date = row.xpath('./td[1]//a/text()').extract_first()
+                print('except issue_date')
 
             item = DiscogsItem()
             item['issue_date'] = issue_date
